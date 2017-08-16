13ff6750dde4be96ea8356677d090302c19a0750
kazi <kazi-Imran.Aziz@adviqo.com>
2017-08-16 13:52:52 +0200

    Forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,25 +3,34 @@ import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
 
-  renderTitleField(field)
+  renderField(field)
   {
 
     return (
-      <div>
-
-        <input type="text" {...field.input}/>
-
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input}/>
+        {field.meta.touched?field.meta.error:''}
       </div>
 
     );
   }
 
+  onSubmit(values){
+    console.log(values)
+
+  }
+
   render()
   {
-    return (
-      <form>
-        <Field name="title" component={this.renderTitleField}/>
 
+    const {handleSubmit}=this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Title" name="title" component={this.renderField}/>
+        <Field label="Categories" name="categories" component={this.renderField}/>
+        <Field label="Post Content" name="content" component={this.renderField}/>
+        <button type="submit" className="btn btn-primary">Submit</button>
       </form>
 
     );
@@ -30,4 +39,21 @@ class PostsNew extends Component {
 
 }
 
-export default reduxForm({form: 'PostsNewForm'})(PostsNew);
+function validate(values) {
+
+  const error = {};
+  if (!values.title || values.title.length<3) {
+    error.title = "Length must be atleast three characters";
+  }
+  if (!values.categories) {
+    error.categories = "Please Enter some categories";
+  }
+  if (!values.content) {
+    error.content = "Please Enter some content";
+  }
+
+  return error;
+
+}
+
+export default reduxForm({validate, form: 'PostsNewForm'})(PostsNew);
