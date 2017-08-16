efb699b5853db2379a14d9b99019e35f550be7d9
javakiddo <kiaziz2002@hotmail.com>
2017-08-16 20:57:47 +0200

    react form post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,36 +1,53 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions';
 
 class PostsNew extends Component {
 
   renderField(field)
   {
+    const {
+      meta: {
+        touched,
+        error
+      }
+    } = field;
+    const className = `form-group ${touched && error
+      ? 'has-danger'
+      : ''}`;
 
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input}/>
-        {field.meta.touched?field.meta.error:''}
+        <div className="text-help">
+          {touched
+            ? error
+            : ''}
+        </div>
       </div>
 
     );
   }
 
-  onSubmit(values){
-    console.log(values)
+  onSubmit(values) {
+this.props.createPost(values);
 
   }
 
   render()
   {
 
-    const {handleSubmit}=this.props;
+    const {handleSubmit} = this.props;
     return (
       <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderField}/>
         <Field label="Categories" name="categories" component={this.renderField}/>
         <Field label="Post Content" name="content" component={this.renderField}/>
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link className="btn btn-warning-outline mx-auto" to="/">Go Back</Link>
       </form>
 
     );
@@ -42,7 +59,7 @@ class PostsNew extends Component {
 function validate(values) {
 
   const error = {};
-  if (!values.title || values.title.length<3) {
+  if (!values.title || values.title.length < 3) {
     error.title = "Length must be atleast three characters";
   }
   if (!values.categories) {
@@ -56,4 +73,4 @@ function validate(values) {
 
 }
 
-export default reduxForm({validate, form: 'PostsNewForm'})(PostsNew);
+export default reduxForm({validate, form: 'PostsNewForm'})(connect(null, {createPost})(PostsNew));
